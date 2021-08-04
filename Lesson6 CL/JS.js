2d24911d15c88f9316b03dc754225d66f59634a0
Tarik09 <nudga.t4@gmail.com>
2021-08-04 22:49:44 +0300

    Commit

---
+++ Lesson6 CL/JS.js
@@ -0,0 +1,33 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(users => {
+
+
+        let usersBox = document.getElementsByClassName('users-box') [0];
+        for (const user of users) {
+            let pUser = document.createElement('p');
+            pUser.innerText = `${user.id} - ${user.title}`;
+            let detalisBtn = document.createElement('button');
+            detalisBtn.innerText = 'Details';
+            detalisBtn.onclick = function () {
+                console.log('print of posts of user', user.id);
+
+                fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+                    .then(value => value.json())
+                    .then(posts => {
+
+                        let postsBox = document.getElementsByClassName('posts-box') [0];
+                        postsBox.innerHTML = '';
+                        for (const post of posts) {
+                            let liPost = document.createElement('li');
+                            liPost.innerText = `${post.body}`;
+                            postsBox.append(liPost);
+
+                        }
+
+                    })
+            };
+            pUser.appendChild(detalisBtn);
+            usersBox.appendChild(pUser);
+        }
+    })
