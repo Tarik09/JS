1f661d019930ff3895ad98fe0d9bbf94b9954482
Tarik09 <nudga.t4@gmail.com>
2021-08-07 23:40:43 +0300

    Commit

---
+++ project/main.js
@@ -0,0 +1,19 @@
+const mainDiv = document.getElementById('users');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(value => {
+        for (const user of value) {
+            let userDiv = document.createElement('div');
+            let detailsLink = document.createElement('a');
+
+            userDiv.innerText = `${user.id} ${user.name}`
+            detailsLink.innerText = 'Details';
+
+            detailsLink.href = `user-details.html?user=${JSON.stringify(user)}`;
+
+            userDiv.appendChild(detailsLink);
+            mainDiv.appendChild(userDiv);
+        }
+
+    })
